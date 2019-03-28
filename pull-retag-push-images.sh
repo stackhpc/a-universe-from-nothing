57f2a369bc6ceac0038067664036fadff79e31d2
Mark Goddard <mark@stackhpc.com>
2019-03-28 11:44:01 +0000

    Use a full local registry rather than a mirror
    This ensures that we won't pull down new images from Dockerhub.
    
    Also adds a pull-retag-push-images.sh script.

---
+++ pull-retag-push-images.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+tag=rocky
+images="kolla/centos-binary-kolla-toolbox
+kolla/centos-binary-haproxy
+kolla/centos-binary-mariadb
+kolla/centos-binary-fluentd
+kolla/centos-binary-cron
+kolla/centos-binary-keepalived
+kolla/centos-binary-neutron-server
+kolla/centos-binary-neutron-l3-agent
+kolla/centos-binary-neutron-metadata-agent
+kolla/centos-binary-neutron-openvswitch-agent
+kolla/centos-binary-neutron-dhcp-agent
+kolla/centos-binary-glance-api
+kolla/centos-binary-nova-compute
+kolla/centos-binary-keystone-fernet
+kolla/centos-binary-keystone-ssh
+kolla/centos-binary-keystone
+kolla/centos-binary-nova-placement-api
+kolla/centos-binary-nova-api
+kolla/centos-binary-nova-consoleauth
+kolla/centos-binary-nova-conductor
+kolla/centos-binary-nova-ssh
+kolla/centos-binary-nova-novncproxy
+kolla/centos-binary-nova-scheduler
+kolla/centos-binary-openvswitch-vswitchd
+kolla/centos-binary-openvswitch-db-server
+kolla/centos-binary-nova-libvirt
+kolla/centos-binary-memcached
+kolla/centos-binary-rabbitmq
+kolla/centos-binary-chrony
+kolla/centos-binary-heat-api
+kolla/centos-binary-heat-engine
+kolla/centos-binary-horizon
+kolla/centos-source-bifrost-deploy"
+registry=192.168.33.5:4000
+
+for image in $images; do
+    ssh 192.168.33.5 sudo docker pull $image:$tag
+    ssh 192.168.33.5 sudo docker tag $image:$tag $registry/$image:$tag
+    ssh 192.168.33.5 sudo docker push $registry/$image:$tag
+done
