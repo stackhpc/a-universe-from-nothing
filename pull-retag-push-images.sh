16767620d44cfd4090f22c8edac904189c644377
Pierre Riteau <pierre@stackhpc.com>
2020-08-21 15:17:00 +0200

    Fix pull-retag-push script when multiple regexes are used

--- pull-retag-push-images.sh
+++ pull-retag-push-images.sh
@@ -5,11 +5,10 @@ set -e
 PARENT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 KAYOBE_PATH="$PARENT/../../../"
 
-ARGS="$@"
 if [[ -n $1 ]]; then
     KAYOBE_EXTRA_ARGS="-e container_image_regexes=\"$@\""
 fi
 
 cd ${KAYOBE_PATH}
 source dev/environment-setup.sh
-kayobe playbook run ${KAYOBE_CONFIG_PATH}/ansible/pull-retag-push.yml ${KAYOBE_EXTRA_ARGS}
+kayobe playbook run ${KAYOBE_CONFIG_PATH}/ansible/pull-retag-push.yml "${KAYOBE_EXTRA_ARGS}"
