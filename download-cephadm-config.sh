983dff635ade12d49e122c10e6aa9526d5b4e8a8
Stig Telfer <stig@stackhpc.com>
2024-10-08 18:37:39 +0100

    Update for Antelope

--- download-cephadm-config.sh
+++ download-cephadm-config.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-BASE_URL="https://raw.githubusercontent.com/stackhpc/stackhpc-kayobe-config/stackhpc/yoga/etc/kayobe"
-DIR1="$HOME/kayobe/config/src/kayobe-config/etc/kayobe/ansible"
-DIR2="$HOME/kayobe/config/src/kayobe-config/etc/kayobe"
+if [[ -z "$KAYOBE_CONFIG_PATH" ]]
+then
+    echo "Run this script in a Kayobe environment, with KAYOBE_CONFIG_PATH set" >&2
+    exit -1
+fi
+
+BASE_URL="https://raw.githubusercontent.com/stackhpc/stackhpc-kayobe-config/stackhpc/2023.1/etc/kayobe"
 
 # Download ansible playbooks
-curl -o $DIR1/cephadm-commands-post.yml $BASE_URL/ansible/cephadm-commands-post.yml
-curl -o $DIR1/cephadm-commands-pre.yml $BASE_URL/ansible/cephadm-commands-pre.yml
-curl -o $DIR1/cephadm-crush-rules.yml $BASE_URL/ansible/cephadm-crush-rules.yml
-curl -o $DIR1/cephadm-deploy.yml $BASE_URL/ansible/cephadm-deploy.yml
-curl -o $DIR1/cephadm-ec-profiles.yml $BASE_URL/ansible/cephadm-ec-profiles.yml
-curl -o $DIR1/cephadm-gather-keys.yml $BASE_URL/ansible/cephadm-gather-keys.yml
-curl -o $DIR1/cephadm-keys.yml $BASE_URL/ansible/cephadm-keys.yml
-curl -o $DIR1/cephadm-pools.yml $BASE_URL/ansible/cephadm-pools.yml
-curl -o $DIR1/cephadm.yml $BASE_URL/ansible/cephadm.yml
+curl -o $KAYOBE_CONFIG_PATH/ansible/cephadm-commands-post.yml $BASE_URL/ansible/cephadm-commands-post.yml
+curl -o $KAYOBE_CONFIG_PATH/ansible/cephadm-commands-pre.yml $BASE_URL/ansible/cephadm-commands-pre.yml
+curl -o $KAYOBE_CONFIG_PATH/ansible/cephadm-crush-rules.yml $BASE_URL/ansible/cephadm-crush-rules.yml
+curl -o $KAYOBE_CONFIG_PATH/ansible/cephadm-deploy.yml $BASE_URL/ansible/cephadm-deploy.yml
+curl -o $KAYOBE_CONFIG_PATH/ansible/cephadm-ec-profiles.yml $BASE_URL/ansible/cephadm-ec-profiles.yml
+curl -o $KAYOBE_CONFIG_PATH/ansible/cephadm-gather-keys.yml $BASE_URL/ansible/cephadm-gather-keys.yml
+curl -o $KAYOBE_CONFIG_PATH/ansible/cephadm-keys.yml $BASE_URL/ansible/cephadm-keys.yml
+curl -o $KAYOBE_CONFIG_PATH/ansible/cephadm-pools.yml $BASE_URL/ansible/cephadm-pools.yml
+curl -o $KAYOBE_CONFIG_PATH/ansible/cephadm.yml $BASE_URL/ansible/cephadm.yml
 
 # Download the cephadm config file
-curl -o $DIR2/cephadm.yml https://raw.githubusercontent.com/stackhpc/a-universe-from-nothing/cephadm-role/etc/kayobe/cephadm.yml.template
+curl -o $KAYOBE_CONFIG_PATH/cephadm.yml https://raw.githubusercontent.com/stackhpc/a-universe-from-nothing/cephadm-role/etc/kayobe/cephadm.yml.template
 
 echo "Download of cephadm files complete!"
